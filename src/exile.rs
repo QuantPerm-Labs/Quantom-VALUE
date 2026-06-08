d36ad1fe2a69b8ad3cb28bfa5a71eb7532374a59
floriddasoftware <floriddasoftware@gmail.com>
2026-06-08 01:07:54 -0700

    Update exile.rs

--- src/exile.rs
+++ src/exile.rs
@@ -27,11 +27,12 @@ impl QuantPerm {
             );
 
         let to = inverse.as_u128() as Dimension;
+        let mirror_bytes = inverse.bytes()
 
         // 3. Fresh inverse physics derived purely from internal state mass
         let (tau, delta, gross_work,
         ) = Self::calculate_work(
-            euclid,
+            mirror_bytes,
             self.retained_mass,
             from,
             to,
